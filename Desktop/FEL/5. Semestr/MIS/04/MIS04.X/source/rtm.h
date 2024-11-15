860468c5f06a8fef4734010e0abe96e6cce4fd7d
prazaon2 <ondra.pazak@gmail.com>
2024-11-15 15:56:02 +0100

    Zacatek psani ukolu RTM - zalozeni hlavicky pro funnkci rtm

---
+++ Desktop/FEL/5. Semestr/MIS/04/MIS04.X/source/rtm.h
@@ -0,0 +1,13 @@
+//Hlavicka funkce rtm
+
+
+#ifndef _RTM_H    /* Guard against multiple inclusion */
+#define _RTM_H
+
+
+#define One_Int_Len 3    //definuju delku zpravy jednoho cisla pro potenciomet
+#define Two_Int_Len 5   //definuju delku zpravy dvou cisel pro tlacitka s pameti
+
+
+
+#endif 
