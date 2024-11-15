d8f588b46a0fa3198cb718ae532f98bd82f164da
prazaon2 <ondra.pazak@gmail.com>
2024-11-15 16:01:05 +0100

    Priprava funkce rtm

---
+++ Desktop/FEL/5. Semestr/MIS/04/MIS04.X/source/rtm.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.c
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: File Scope or Global Data                                         */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+/* ************************************************************************** */
+/** Descriptive Data Item Name
+
+  @Summary
+    Brief one-line summary of the data item.
+    
+  @Description
+    Full description, explaining the purpose and usage of data item.
+    <p>
+    Additional description in consecutive paragraphs separated by HTML 
+    paragraph breaks, as necessary.
+    <p>
+    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+    
+  @Remarks
+    Any additional remarks
+ */
+int global_data;
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Local Functions                                                   */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+#include "messengerMIS.h"
+#include "rtm.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+unsigned char recBuf[40];
+unsigned char sendBuf[40];
+
+
+void messenger(bool mem_S1, bool mem_S2) { 
+    
+    
+            
+            
+}
+        
+        
+        
+        
+        
+        
+        
+        
+    
+           
+   
