5b51803ee4c9c896724f5ce15fafbce93c6545a4
prazaon2 <ondra.pazak@gmail.com>
2024-11-15 16:26:38 +0100

    Pridani komentaru

--- Desktop/FEL/5. Semestr/MIS/04/MIS04.X/source/rtm.c
+++ Desktop/FEL/5. Semestr/MIS/04/MIS04.X/source/rtm.c
@@ -72,36 +72,37 @@ int global_data;
 #include <stdbool.h>
 #include <stdio.h>
 
-unsigned char recBuf[40];
-unsigned char sendBuf[40];
+unsigned char recBuf[40];  //inicializace pole prichozi zpravy
+unsigned char sendBuf[40];  //inicializace pole obsahujiciho odesilanou zpravu
 
 
 void messenger(bool mem_S1, bool mem_S2) { 
     
-    static int delay = 0;
+    static int delay = 0; 
     static char delka = 0;
     static char typ = 0;
   
     
-    delay = delay+1;
+    delay = delay+1; //inkcementace
     
     if(delay >= 40){
         
-        getMessageUSB(recBuf,0);
-            delka = recBuf[0];
-            typ = recBuf[1];
-            switch (typ){
-                case 0:{
+        getMessageUSB(recBuf,0); //pripravena funkce - prijme zpravu z PC a uloziji do pole recBuf
+            delka = recBuf[0];  // na pozici 0 se nachazi informace o delce zpravy. Bude se hodit kdyz budu chtit pridat kontrolu prijate zpravy
+            typ = recBuf[1];    //na pozici 1 se nachazi informace o typu prijate zpravy => jaky COM jsem vybral na PC
+            
+            switch (typ){   
+                case 0:{    //COM0 preruseni komunikace
                     break;
                 }
-                case 1:
+                case 1:         //COM1 odesilej hodnotu potenciometru
                 {
                     sendBuf[0]= One_Int_Len;
                     integerToBytes(getPotentiometerValue(),&sendBuf[1]);
                     sendMessageUSB(sendBuf,0);
                     break;
                 }
-                case 2:
+                case 2:         //COM2 odesilej hodnotu tlacitek s pameti
                 {
                     sendBuf[0]= Two_Int_Len;
                     integerToBytes(mem_S1,&sendBuf[1]);
@@ -109,7 +110,7 @@ void messenger(bool mem_S1, bool mem_S2) {
                     sendMessageUSB(sendBuf,0);
                     break;
                 }
-                case 3:
+                case 3:     //COM3 tabulka
                 {
                     sprintf(sendBuf,"Hodnota AD = %d", getPotentiometerValue());
                     sendTableTerminalMessageUSB("1A",sendBuf);
