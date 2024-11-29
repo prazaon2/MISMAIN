e0fe6f1fcfe2aed1dca31aff41a81c50a1e65860
prazaon2 <ondra.pazak@gmail.com>
2024-11-29 13:54:35 +0100

    Priprava pro funkci prepoctu potenciometru

---
+++ Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/potenciometr.c
@@ -0,0 +1,37 @@
+
+/* ************************************************************************** */
+/** Descriptive Data Item Name
+
+  @Summary
+    Brief one-line summary of the data item.
+    
+  @Description
+    Full description, explaining the purpose and usage of data item.
+    <p>
+    Additional description in consecutive paragraphs separated by HTML 
+    paragraph breaks, as necessary.
+    <p>
+    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+    
+  @Remarks
+    Any additional remarks
+ */
+#include "potenciometr.h"
+
+
+
+
+
+void potenciometr(int* hodnota_pot_u) { 
+    
+
+    
+}
+
+/* ****************
+ 
+ 
+ 
+ *************************************************************
+ End of File
+ */
---
+++ Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/potenciometr.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef _POTENCIOMETR_H    /* Guard against multiple inclusion */
+#define _POTENCIOMETR_H
+
+
+#include "platformMIS32mk.h"
+
+    
+void potenciometr(int* hodnota_pot_u);
+
+
+
+
+
+#endif /* _FILTR_H */
+
+/* *****************************************************************************
+ End of File
+ */
