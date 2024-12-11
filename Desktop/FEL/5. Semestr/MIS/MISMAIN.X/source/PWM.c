b521cb21f5d4e39e8ed664f2a16455099da12ee2
prazaon2 <ondra.pazak@gmail.com>
2024-12-11 17:15:00 +0100

    Konfigurace casovace

--- Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/PWM.c
+++ Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/PWM.c
@@ -18,7 +18,11 @@
 #include "PWM.h"
 
 void iniCasovac (void){
-    
+    T2CON = 0;      // zastavi casovac
+    T2CONbits.TCS = 0; // nastavi interni hodnoty jako zdroj ticku
+    T2CONbits.TCKPS = 0b101;    // nasvavy prescaler na 1:32
+    TMR2 = 0;               // "vypne timer count registr" (moc nerozumim co presne dela)
+    PR2 = 37499;        //nastvaeni doby periody (20 ms) s ohledem na 60MHz a 32 prescaler
 }
 
 
