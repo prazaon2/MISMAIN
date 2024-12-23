7f2bcacb6b53b036823c4db55692372cf4016926
prazaon2 <ondra.pazak@gmail.com>
2024-12-23 17:59:35 +0100

    Implementace informaci pro RTM a realizace stavu TEST

--- Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/plc.c
+++ Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/plc.c
@@ -14,6 +14,7 @@
     Describe the purpose of this file.
  */
 #include "plc.h"
+#include "prepocet_stupne.h"
 
 
 
@@ -21,22 +22,29 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
     
     int static pocet_zapsani;
     int static index = 0;
-    int static index2 = 0;
+    int static indexRUN = 0;
+    int static indexTEST = 0;
     int static zapsano = 1;
     static int delay = 0;
+    static int delayTEST = 0;
     static int SETpole[10];
     
     switch(plc->stav){
         
         case 0: //PROG
         {
-            index2 = 0;
+            plc->rtm_stav = 0;
+            indexRUN = 0;
+            indexTEST = 0;
             if(vstupS7 == 1 && index < 10 && zapsano == 1){
                 zapsano = 0;
+                plc->rtm_zatezovatel = prepocet_stupne(vstup_zatezovatel);
+                plc->rtm_index = index;
+                
                 switch(index){
                     case 0:
                     {
-                        SETpole[0] = vstup_zatezovatel;
+                        SETpole[0] = vstup_zatezovatel;                        
                         break;
                     }
                     case 1:
@@ -114,18 +122,22 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
         
         case 1: //RUN
         {
+            plc->rtm_stav = 1;
             pocet_zapsani = index;
             index = 0;
+            indexTEST = 0;
             
             delay++;
-            if(delay >= 1000){
+            if(delay >= 2000){
                 
-                OC16RS = ((SETpole[index2]/(255.0))*1875)+1875;
-                index2++;
+                plc->rtm_index = indexRUN;
+                plc->rtm_zatezovatel = prepocet_stupne(SETpole[indexRUN]);
+                OC16RS = ((SETpole[indexRUN]/(255.0))*1875)+1875;
+                indexRUN++;
                 delay=0;
             }
-            if(index2 >= pocet_zapsani){
-                index2 = 0;
+            if(indexRUN > pocet_zapsani){
+                indexRUN = 0;
             }
             
             if(vstupS6 == 1){
@@ -140,6 +152,10 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
         
         case 2: //STOP
         {
+            plc->rtm_stav = 2;
+            plc->rtm_index = indexRUN;
+            plc->rtm_zatezovatel = prepocet_stupne(SETpole[indexRUN]);
+            
             if(vstupS5 == 1){
                 plc->stav = 1;
             }
@@ -162,14 +178,32 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
         }
         case 4: //TEST
         {
+            indexRUN = 0;
+            index = 0;
             
+            delayTEST++;
+            if(delayTEST >= 2000){
+                
+                plc->rtm_index = indexTEST;
+                plc->rtm_zatezovatel = prepocet_stupne(SETpole[indexTEST]);                
+                indexTEST++;
+                delayTEST = 0;
+            }
+            if(indexTEST > pocet_zapsani){
+                indexTEST = 0;
+            }
             
+            if(vstupS8 == 1){
+                indexTEST = 0;
+            }
             
-        if(vstupS5 == 1){
+            plc->rtm_stav = 4;
+            
+            if(vstupS5 == 1){
                 plc->stav = 1;
             }
         
-        if(vstupS6 == 1){
+            if(vstupS6 == 1){
                 plc->stav = 3;
             }
         
