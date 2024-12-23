49c11038c5d448cc8fd7cdc5f069af675e176096
prazaon2 <ondra.pazak@gmail.com>
2024-12-23 16:53:49 +0100

    Pridani funkcnosti pro SET a RESET(nutno zkontrolovat stavovy automat). Prvidani pocet_zapsani aby v modu RUN kolovaly pouze zapsane hodnoty a ne nulove policka v poli.

--- Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/plc.c
+++ Desktop/FEL/5. Semestr/MIS/MISMAIN.X/source/plc.c
@@ -19,6 +19,7 @@
 
 void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6, int vstupS7, int vstupS8) { 
     
+    int static pocet_zapsani;
     int static index = 0;
     int static index2 = 0;
     int static zapsano = 1;
@@ -108,11 +109,22 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
                 plc->stav = 1;
             }
             
+            if(vstupS8 == 1){
+                plc->stav = 4;
+            }
+            
+            if(vstupS6 == 1){
+                plc->stav = 3;
+            }
+            
+           
+            
             break;
         }   
         
         case 1: //RUN
         {
+            pocet_zapsani = index;
             index = 0;
             
             delay++;
@@ -122,10 +134,13 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
                 index2++;
                 delay=0;
             }
-            if(index2 > 9){
-                index = 0;
+            if(index2 > pocet_zapsani){
+                index2 = 0;
             }
             
+            if(vstupS6 == 1){
+                plc->stav = 3;
+            }
             if(vstupS4 == 1){
                 plc->stav = 2;
             }
@@ -135,10 +150,39 @@ void plc(plc_* plc, int vstup_zatezovatel, int vstupS4, int vstupS5, int vstupS6
             
             break;
         }
-        case 2:
+        case 2: //STOP
         {
+            if(vstupS5 == 1){
+                plc->stav = 1;
+            }
+            break;
+        }
+        case 3:  //RESET
+        {
+            plc->h1 = 0;
+            plc->h2 = 0;
+            plc->h3 = 0;
+            plc->h4 = 0;
+            plc->h5 = 0;
+            plc->h6 = 0;
+            plc->h7 = 0;
+            plc->h8 = 0;
+            plc->h9 = 0;
+            plc->h10 = 0;
+            
+            for(int i = 0; i<10; i++){
+                SETpole[i] = 0;
+            }
+            
+            plc->stav = 0;
             
             break;
+        }
+        case 4: //TEST
+        {
+            
+        
+        
         }
     }
 }
